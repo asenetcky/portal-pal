c0f7742acf7e83ee44f8c74d3a8ded4bbe812808
asenetcky <me@asenetcky.dev>
2026-06-22 21:11:07 -0400

    feat: init etl skeleton code

---
+++ apps/etl/src/etl/main.py
@@ -0,0 +1,16 @@
+from sodapy import Socrata
+
+
+class domain:
+    """open data portal domain"""
+
+    name: str
+
+    # TODO: add methods for pulling domain info down
+
+
+def main(): ...
+
+
+if __name__ == "__main__":
+    main()
