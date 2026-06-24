3fc2f6d714feb2989e51047349640b39ab036ad7
asenetcky <me@asenetcky.dev>
2026-06-23 21:21:44 -0400

    chore: quick proof of concept

--- apps/etl/src/etl/main.py
+++ apps/etl/src/etl/main.py
@@ -9,7 +9,16 @@ class domain:
     # TODO: add methods for pulling domain info down
 
 
-def main(): ...
+def pull_domain_data():
+    """Pull down domain level metadata"""
+
+    with Socrata("data.ct.gov", None) as client:
+        datasets = client.datasets()
+        print(datasets[0:5])
+
+
+def main():
+    pull_domain_data()
 
 
 if __name__ == "__main__":
