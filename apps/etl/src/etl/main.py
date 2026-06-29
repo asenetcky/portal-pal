fb853c32e5011ffef870b21500d0afb2bc4c1aa2
asenetcky <me@asenetcky.dev>
2026-06-29 18:06:13 -0400

    feat: add base of etl scrape

--- apps/etl/src/etl/main.py
+++ apps/etl/src/etl/main.py
@@ -1,24 +1,41 @@
+import datetime as dt
+from uuid import UUID, uuid4
+from supabase import create_client, Client
 from sodapy import Socrata
+from backend.config import get_settings
+from pydantic import BaseModel, Field
+from typing import Any
 
 
-class domain:
-    """open data portal domain"""
+class PortalMetaData(BaseModel):
+    """Metadata on the Open Data Portal"""
 
-    name: str
+    domain: str 
+    id: UUID = Field(default_factory=uuid4)
+    timestamp: str = Field(default_factory=lambda: dt.datetime.now(tz=dt.UTC).isoformat())
+    payload: list[dict[str, Any]]
 
-    # TODO: add methods for pulling domain info down
+    @classmethod
+    def from_domain(cls, domain:str) -> "PortalMetaData":
+        """
+        Fetches the metadata from the domain and returns an initialized
+        PortalMetaData instance.
+        """
+        settings = get_settings()
+
+        with Socrata(domain, settings.portal_pal_api_key.get_secret_value()) as client:
+            datasets = client.datasets()
+
+            return cls(
+                domain = domain,
+                payload = datasets
+            )
 
 
-def pull_domain_data():
-    """Pull down domain level metadata"""
-
-    with Socrata("data.ct.gov", None) as client:
-        datasets = client.datasets()
-        print(datasets[0:5])
 
 
 def main():
-    pull_domain_data()
+    my_payload = PortalMetaData.from_domain("data.ct.gov")
 
 
 if __name__ == "__main__":
